fa5cd0a97ea746f873b2907f3e4a474016533dda
Andi <mab.suwandi@gmail.com>
2019-05-20 22:19:56 +0700

    update exercise-17

--- exercise-17.js
+++ exercise-17.js
@@ -3,42 +3,25 @@
 
 function kaliTerusRekursif(angka) {
   // you can only write your code here!
-  var strAngka = String(angka),
-      result = parseInt(strAngka[0]);
-  console.log(strAngka, ' --> strAngka')
-  console.log(result, ' --> result')
-  if (strAngka.length == 0) {
-    return result = 1;
+  var strResult = String(angka),
+      str = '';
+  if (strResult.length == 1) {
+    return parseInt(strResult);
   } else {
-    var irisan = parseInt(strAngka.slice(1));
-    console.log(irisan, ' --> irisan')
-    return result * kaliTerusRekursif(irisan);
-  }
-}
-
-
-// ---- code 2 -----------------------------------------------
-
-function kaliTerusRekursif(angka) {
-  // you can only write your code here!
-  var strNum = String(angka),
-      result = parseInt(strNum[0]),
-      strResult = String(result),
-      strArg = strNum.substr(1);
-  if(strResult.length == 1 && strArg.length == 0) {
-    return 1;
-  } else if (strArg.length > 1 && strArg.length == 0) {
-    nexArg = strArg; 
-  } else {
-    nexArg = parseInt(strNum.substr(1));
-    console.log(result, ' --> result')
-  }
-  return result * kaliTerusRekursif(nexArg);
+    str = strResult[0] * strResult[1];
+    if (String(str).length > 1 && strResult.length > 2) {
+      str = str * strResult[2];
+      return kaliTerusRekursif(parseInt(str + strResult.substr(3)));
+    } else if (strResult.length > 2) {
+      return kaliTerusRekursif(parseInt(str + strResult.substr(2)));
+    }
+    return kaliTerusRekursif(str);
+  } 
 }
 
 // TEST CASES
 console.log(kaliTerusRekursif(66)); // 8
-// console.log(kaliTerusRekursif(3)); // 3
-// console.log(kaliTerusRekursif(24)); // 8
-// console.log(kaliTerusRekursif(654)); // 0
-// console.log(kaliTerusRekursif(1231)); // 6
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
